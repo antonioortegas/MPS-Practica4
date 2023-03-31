d76bb6d259369b2e248f0b9ca2808d38ce3857ee
antonioortegas <antonioortega@uma.es>
2023-03-31 03:34:15 +0200

    init

---
+++ src/test/java/org/mps/plateau/PlateauLineCoverageTest.java
@@ -0,0 +1,23 @@
+package org.mps.plateau;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlateauLineCoverageTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void test(){
+        assertEquals(1,1);
+    }
+}
